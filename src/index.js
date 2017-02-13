63f70223c708f36d1cfcde8b5532868dc400bea7
kristine <ktomlinson@elltechnologies.com>
2017-02-13 18:28:00 -0500

    Added mock data generation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './index.css';
+import {getUsers} from './api/userApi';
+
 debugger; // eslint-disable-line no-debugger
 console.log('Hello World!'); // eslint-disable-line no-console
+
+getUsers().then(result => {
+    let userBody = '';
+    result.forEach(user => {
+        userBody += `<tr>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.email}</td>
+        </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = userBody;
+});
